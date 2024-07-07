467b526997475f60b20624e44375d883cbb9fc67
Krystal Lake <krystal.lake10@gmail.com>
2024-07-07 13:06:21 +0100

    Basic Dom practice

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+
+document.write("Some Text added by Javascript")
+
+const embeddedDiv = document.getElementById("embedded_div")
+
+console.log(embeddedDiv)
+
+embeddedDiv.append(" Hello World")
