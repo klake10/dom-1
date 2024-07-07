3f32833cbeeeb9cbf26a4be6258a9a660f22dc98
Krystal Lake <krystal.lake10@gmail.com>
2024-07-07 13:46:38 +0100

    Added a counter variable

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const embeddedDiv = document.getElementById("embedded_div")
 console.log(embeddedDiv)
 
 embeddedDiv.append(" Hello World")
+
+let count = 0
+
+const button = document.getElementById("click_me_button")
+
+button.addEventListener("click", () => {
+    count ++
+    console.log(count)
+})
